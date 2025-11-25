f173b01c534a28971bdcc30eb33e1810e6dc282e
John Mc Queide <mcqueide@gmail.com>
2025-11-25 18:44:04 -0300

    Fixed ViewerModule.forRoot issue.

--- apps/total-angular/src/app/app.module.ts
+++ apps/total-angular/src/app/app.module.ts
@@ -7,19 +7,27 @@ import {TotalViewComponent} from './total-view/total-view.component';
 import {ViewerModule, ViewerAppComponent} from '@groupdocs.examples.angular/viewer';
 import {AnnotationModule, AnnotationAppComponent} from "@groupdocs.examples.angular/annotation";
 
+// Call forRoot outside the @NgModule so the imports array contains only module references
+const viewerModuleWithProviders = ViewerModule.forRoot("http://localhost:8080");
+const annotationModuleWithProviders = AnnotationModule.forRoot("http://localhost:8081");
+
 @NgModule({
   declarations: [AppComponent, TotalNavComponent, TotalViewComponent],
   imports: [
     BrowserModule,
-    ViewerModule.forRoot("http://localhost:8080"),
-    AnnotationModule.forRoot("http://localhost:8081"),
+    ViewerModule,
+    AnnotationModule,
     RouterModule.forRoot([
       {path: '', component: TotalViewComponent},
       {path: 'viewer', component: ViewerAppComponent},
       {path: 'annotation', component: AnnotationAppComponent},
     ], {initialNavigation: 'enabled'})
   ],
-  providers: [],
+  providers: [
+    // spread the providers returned by forRoot
+    ...(viewerModuleWithProviders && (viewerModuleWithProviders as any).providers || []),
+    ...(annotationModuleWithProviders && (annotationModuleWithProviders as any).providers || [])
+  ],
   exports: [
     TotalViewComponent
   ],
